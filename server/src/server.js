6014c877a2a98b15036ff9989f5ec55d879eb134
adityawikiance <aditya.dixit@wikiance.com>
2025-06-25 21:50:00 +0530

    "s"

--- server/src/server.js
+++ server/src/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN ,
+  origin: "https://work-loop-pink.vercel.app",
   credentials: true,
 }));
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
